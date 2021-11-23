36c0cc783044cb7e370ccad5fd8da05b7f51ddb5
Syed Salman Reza <71028588+syed-reza98@users.noreply.github.com>
2021-11-24 03:55:29 +0600

    Added Header and footer for orders page

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,1 +1,15 @@
-Orders
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('View Orders') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                View All Orders
+            </div>
+        </div>
+    </div>
+</x-app-layout>
