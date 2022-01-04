17bea386d0d55da93a3265264a6bc4a8fd7faa69
Syed Salman Reza <71028588+syed-reza98@users.noreply.github.com>
2022-01-04 23:42:48 +0600

    Created Orders table

---
+++ database/migrations/2022_01_01_135458_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->float('total')->default( 0 );
+            $table->integer('quantity');
+            $table->enum('payment_status',['paid','unpaid'])->default('unpaid');
+            $table->enum('status',['new','process','delivered','cancel'])->default('new');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('bkash');
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address1');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
